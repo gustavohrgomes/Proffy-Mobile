7659e1bb33cf7e03eea3532a115fb51e5a040491
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-08-08 18:18:24 -0300

    Import PageHeader Component in Favorites & TeacherList pages

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function Favorites() {
   return (
-    <View style={styles.conatiner}/>
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys Favoritos"/>
+    </View>
   );
 }
 
--- src/pages/TeacherList/styles.ts
+++ src/pages/TeacherList/styles.ts
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  conatiner: {
+  container: {
     flex: 1,
     backgroundColor: '#f0f0f7',
   }
